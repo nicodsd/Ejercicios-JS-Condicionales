0370eb87025ac15290e8099705441bea0154082a
unknown <nilonb7@gmail.com>
2023-04-02 20:54:53 -0300

    ejercicios-v01

---
+++ app.js
@@ -0,0 +1,48 @@
+// Ejercitación:
+
+//1. Realizar un programa que dado 2 números imprima por consola si el primer numero es
+//mayor que el segundo.
+
+/* //Respuesta 1
+//Ingrese un numero en los parametros:
+let x = 3;
+let y = 4;
+//
+    if (x > y){
+        console.log(`Este numero "${x}" es mayor a este "${y}"`);
+    }    
+    else {
+        console.log(`Este numero "${x}" es menor a este "${y}"`);
+    }
+*/
+
+
+//2. Realizar un programa que dado 2 números imprima por consola si los numeros son
+//iguales o si son diferentes.
+
+/* //Ingrese un numero en los parametros.
+let x = ;
+let y = ;
+
+    if (x === y){
+        console.log(`Son iguales`);
+    }else { 
+        console.log(`Son distintos`);
+    }
+*/
+
+//3. Realizar un programa que dado 2 números imprima por consola cual de los 2 numeros
+//es el mas grande o si son iguales.
+
+let valorUno = ;
+let valorDos = ;
+
+if ( valorUno === valorDos ){
+    console.log
+}
+
+
+
+function operacion(){
+
+}
